26224079a386ae086990e09c740996672e157ee4
simple-ice <simplewu233@gmail.com>
2020-08-23 05:17:41 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>hello</h1>
+  <Test />
+</template>
+
+<script>
+import Test from './components/Test.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Test
+  }
+}
+</script>
