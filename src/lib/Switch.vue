aa977b8566200fea205e4bd34bfd24a4002c656c
simple-ice <simplewu233@gmail.com>
2020-08-25 02:40:09 +0800

    完成点击button切换

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,24 @@
 <template>
-<button> <span></span> </button>
+<button @click="toggle" :class="{ checked }"><span></span></button>
 </template>
+
+<script>
+import {
+    ref
+} from "vue";
+export default {
+    setup() {
+        const checked = ref(false);
+        const toggle = () => {
+            checked.value = !checked.value;
+        };
+        return {
+            checked,
+            toggle,
+        };
+    },
+};
+</script>
 
 <style lang="scss" scoped>
 $h: 22px;
@@ -8,20 +26,28 @@ $h2: $h - 4px;
 
 button {
     height: $h;
-    width: $h*2;
+    width: $h * 2;
     border: none;
-    background: blue;
+    background: #9f9f9f;
     border-radius: $h/2;
     position: relative;
-}
 
-span {
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background: white;
-    border-radius: $h2 / 2;
+    >span {
+        position: absolute;
+        top: 2px;
+        left: 2px;
+        height: $h2;
+        width: $h2;
+        background: white;
+        border-radius: $h2 / 2;
+    }
+
+    &.checked {
+        background: #027be3;
+    }
+
+    &.checked>span {
+        left: calc(100% - #{$h2} - 2px);
+    }
 }
 </style>
