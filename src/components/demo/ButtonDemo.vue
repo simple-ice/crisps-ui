ad55b1f77e45725c54d797c37933869b2db1519d
simple-ice <simplewu233@gmail.com>
2020-08-25 05:19:26 +0800

    通过theme属性控制button的样式

--- src/components/demo/ButtonDemo.vue
+++ src/components/demo/ButtonDemo.vue
@@ -3,6 +3,9 @@
 <h1>示例1</h1>
 <div>
     <Button>这是一个button</Button>
+    <Button theme="button">这是一个button</Button>
+    <Button theme="link">这是一个button</Button>
+    <Button theme="text">这是一个button</Button>
 </div>
 </template>
 
@@ -11,6 +14,14 @@ import Button from '../../lib/Button.vue'
 export default {
     components: {
         Button
+    },
+    setup() {
+        const onClick = () => {
+            console.log('hello')
+        }
+        return {
+            onClick
+        }
     }
 }
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,16 @@
 <template>
-<button>
+<button class="crisps-button" :class="`theme-${theme}`">
     <slot />
 </button>
 </template>
+
+<script lang="ts">
+export default {
+    props: {
+        theme: {
+            type: String,
+            default: 'button'
+        }
+    }
+}
+</script>
