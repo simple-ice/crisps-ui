ab586e32a2ed526f27cd170a68aa023ee701050b
simple-ice <simplewu233@gmail.com>
2020-08-24 19:11:38 +0800

    topnav 样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,39 @@
 <template>
-    <div>
-        <div class="topnav">
-            <div class="logo"></div>
-            <div class="menu"></div>
-        </div>
-        <div class="banner">
-            <h1>薯片UI</h1>
-            <h2>一个普普通通的 UI 框架</h2>
-            <p class="actions">
-                <a href="">GitHub</a>
-                <a href="">开始</a>
-            </p>
-        </div>
+  <div>
+    <div class="topnav">
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
+    <div class="banner">
+      <h1>薯片UI</h1>
+      <h2>一个普普通通的 UI 框架</h2>
+      <p class="actions">
+        <a href="">GitHub</a>
+        <a href="">开始</a>
+      </p>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-    
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
 </style>
