704957d17fd272c3841f8d4adfae5134f08e69e6
simple-ice <simplewu233@gmail.com>
2020-08-24 21:38:18 +0800

    添加 toggleAside

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="logo" @click="toggleAside">LOGO</div>
+      <div class="logo" >LOGO</div>
       <ul class="menu">
         <li>菜单1</li>
         <li>菜单2</li>
       </ul>
+      <span @click="toggleAside" class="toggleAside"></span>
     </div>
   </div>
 </template>
@@ -29,6 +30,8 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -39,6 +42,27 @@ export default {
     flex-wrap: nowrap;
     > li {
       margin: 0 1em;
+    }
+  }
+  > .toggleAside {
+    display: none;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  @media (max-width: 896px) {
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin: 0 auto;
+    }
+    > .toggleAside{
+        display: inline-block;
     }
   }
 }
