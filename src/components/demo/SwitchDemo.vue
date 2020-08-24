a4bb56fac2365b986b1c0a28d7fa24ebfa151eb8
simple-ice <simplewu233@gmail.com>
2020-08-25 03:09:17 +0800

    可以接收value和input

--- src/components/demo/SwitchDemo.vue
+++ src/components/demo/SwitchDemo.vue
@@ -1,15 +1,23 @@
 <template>
 <div>
-    <Switch />
+    <Switch :value="isValue" @input="isValue = $event" />
 </div>
 </template>
 
 <script lang="ts">
 import Switch from "../../lib/Switch.vue";
+import {
+    ref
+} from 'vue';
 export default {
-    name: "SwitchDemo",
     components: {
         Switch,
     },
+    setup() {
+        const isValue = ref(false)
+        return {
+            isValue
+        }
+    }
 };
 </script>
