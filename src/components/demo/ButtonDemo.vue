68b49a0a423ce6edf946d476018d66d28f9219f4
simple-ice <simplewu233@gmail.com>
2020-08-25 04:32:12 +0800

    初始化Button组件

--- src/components/demo/ButtonDemo.vue
+++ src/components/demo/ButtonDemo.vue
@@ -1,10 +1,16 @@
 <template>
-    <div>
-        Button组件
-    </div>
+<div>Button 组件</div>
+<h1>示例1</h1>
+<div>
+    <Button>这是一个button</Button>
+</div>
 </template>
+
 <script lang="ts">
+import Button from '../../lib/Button.vue'
 export default {
-    name: 'ButtonDemo'
+    components: {
+        Button
+    }
 }
 </script>
