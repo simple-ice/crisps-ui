4db0b5c5d10279f5506b668daa4b96832fb7af04
simple-ice <simplewu233@gmail.com>
2020-08-24 19:38:44 +0800

    修改Home页面的github的链接

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>薯片UI</h1>
       <h2>一个普普通通的 UI 框架</h2>
       <p class="actions">
-        <a href="">GitHub</a>
-        <a href="">开始</a>
+        <a href="https://github.com/simple-ice/crisps-ui">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
