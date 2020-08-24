a6b9b75bf86f45d417a3f5f51d8c3c81dd51d412
simple-ice <simplewu233@gmail.com>
2020-08-24 19:21:38 +0800

    封装 Topnav 组件

---
+++ src/components/Topnav.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+    <Topnav />
     <div class="banner">
       <h1>薯片UI</h1>
       <h2>一个普普通通的 UI 框架</h2>
@@ -17,23 +11,13 @@
     </div>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
+export default {
+    name: 'Home',
+    components:{Topnav}
 }
+</script>
+<style lang="scss" scoped>
+
 </style>
