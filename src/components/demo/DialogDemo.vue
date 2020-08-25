79c5c42b318be0e43fa6c6abf6d7760c1562c04c
simple-ice <simplewu233@gmail.com>
2020-08-26 06:49:18 +0800

    初始化 Dialog 组件

--- src/components/demo/DialogDemo.vue
+++ src/components/demo/DialogDemo.vue
@@ -1,10 +1,14 @@
 <template>
-    <div>
-        Dialog 组件
-    </div>
+<div>Dialog 示例</div>
+<h1>示例1</h1>
+<Dialog> </Dialog>
 </template>
+
 <script lang="ts">
+import Dialog from '../../lib/Dialog.vue'
 export default {
-    name: 'DialogDemo'
+    components: {
+        Dialog
+    }
 }
 </script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="dialog-overlay"></div>
+<div class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+        <p>第一行内容</p>
+        <p>第二行内容</p>
+    </main>
+    <footer>
+        <Button>OK</Button>
+        <Button>Cancel</Button>
+    </footer>
+</div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue'
+export default {
+    components: {
+        Button
+    }
+}
+</script>
