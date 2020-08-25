d8445f61a335998eeb879f3db90b62952b949d78
simple-ice <simplewu233@gmail.com>
2020-08-26 00:46:45 +0800

    支持size属性以及相应的css

--- src/components/demo/ButtonDemo.vue
+++ src/components/demo/ButtonDemo.vue
@@ -2,10 +2,26 @@
 <div>Button 组件</div>
 <h1>示例1</h1>
 <div>
-    <Button>Default</Button>
+    <Button>Default Button</Button>
     <Button theme="button">这是一个button</Button>
     <Button theme="link">Link Button</Button>
     <Button theme="text">Text Button</Button>
+</div>
+<h1>示例2</h1>
+<div>
+    <Button size="big">Big Button</Button>
+    <Button>Normal Button</Button>
+    <Button size="small">Small Button</Button>
+</div>
+<div>
+    <Button size="big" theme="link">Big Button</Button>
+    <Button theme="link">Normal Button</Button>
+    <Button size="small" theme="link">Small Button</Button>
+</div>
+<div>
+    <Button size="big" theme="text">Big Button</Button>
+    <Button theme="text">Normal Button</Button>
+    <Button size="small" theme="text">Small Button</Button>
 </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,15 +1,37 @@
 <template>
-<button class="crisps-button" :class="{[`crisps-theme-${theme}`]: theme}">
+<button class="crisps-button" :class="classes">
     <slot />
 </button>
 </template>
 
 <script lang="ts">
+import {
+    computed
+} from 'vue'
 export default {
     props: {
         theme: {
             type: String,
             default: 'button'
+        },
+        size: {
+            type: String,
+            default: 'normal'
+        }
+    },
+    setup(props) {
+        const {
+            theme,
+            size
+        } = props;
+        const classes = computed(() => {
+            return {
+                [`crisps-theme-${theme}`]: theme,
+                [`crisps-size-${size}`]: size
+            }
+        })
+        return {
+            classes
         }
     }
 }
@@ -75,8 +97,19 @@ $radius: 4px;
         &:hover,
         &:focus {
             background: darken(white, 5%);
-            ;
         }
+    }
+
+    &.crisps-size-big {
+        font-size: 24px;
+        height: 48px;
+        padding: 0 16px;
+    }
+
+    &.crisps-size-small {
+        font-size: 12px;
+        height: 20px;
+        padding: 0 4px;
     }
 }
 </style>
