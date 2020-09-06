6a33a5900f911bcfcbe038c015dcca373dd5b51b
simple-ice <simplewu233@gmail.com>
2020-09-06 20:17:37 +0800

    点击切换内容

--- src/components/demo/TabsDemo.vue
+++ src/components/demo/TabsDemo.vue
@@ -1,13 +1,17 @@
 <template>
 <div>Tabs 示例</div>
 <h1>示例1</h1>
-<Tabs>
+<Tabs v-model:value="name">
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
+    <Tab title="导航3">内容3</Tab>
 </Tabs>
 </template>
 
 <script lang="ts">
+import {
+    ref
+} from 'vue'
 import Tab from "../../lib/Tab.vue";
 import Tabs from "../../lib/Tabs.vue";
 export default {
@@ -15,5 +19,11 @@ export default {
         Tab,
         Tabs,
     },
-};
+    setup() {
+        const name = ref('导航1')
+        return {
+            name
+        }
+    }
+}
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,31 +1,50 @@
 <template>
 <div class="crisps-tabs">
     <div class="crisps-tabs-nav">
-        <div class="crisps-tabs-nav-item" v-for="(t, index) in titles" :key="index">
+        <div class="crisps-tabs-nav-item" @click="select(t)" :class="{ selected: t === value }" v-for="(t, index) in titles" :key="index">
             {{ t }}
         </div>
+        <div class="crisps-tabs-nav-indicator"></div>
     </div>
     <div class="crisps-tabs-content">
-        <component class="crisps-tabs-content-item" v-for="(c, index) in defaults" :is="c" :key="index" />
+        <component class="crisps-tabs-content-item" :is="current" :key="current.props.title" />
     </div>
 </div>
 </template>
 
 <script lang="ts">
+import {
+    computed
+} from "vue";
 import Tab from "./Tab.vue";
 export default {
+    props: {
+        value: {
+            type: String,
+        },
+    },
     setup(props, context) {
         const defaults = context.slots.default();
-        const titles: string[] = [];
         defaults.forEach((tab) => {
             if (tab.type !== Tab) {
                 throw new Error("Tabs 子标签必须为 Tab");
             }
-            titles.push(tab.props.title);
+        });
+
+        const titles = defaults.map((tab) => {
+            return tab.props.title;
+        });
+        const select = (title: string) => {
+            context.emit("update:value", title);
+        };
+        const current = computed(() => {
+            return defaults.find((tab) => tab.props.title === props.value);
         });
         return {
             defaults,
             titles,
+            current,
+            select,
         };
     },
 };
@@ -41,6 +60,7 @@ $border-color: #d9d9d9;
         display: flex;
         color: $color;
         border-bottom: 1px solid $border-color;
+        position: relative;
 
         &-item {
             padding: 8px 0;
@@ -54,6 +74,15 @@ $border-color: #d9d9d9;
             &.selected {
                 color: $blue;
             }
+        }
+
+        &-indicator {
+            position: absolute;
+            height: 3px;
+            background: $blue;
+            left: 0;
+            bottom: -1px;
+            width: 100px;
         }
     }
 
