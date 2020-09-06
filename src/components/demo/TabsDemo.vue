29b45fd06fd7305254524e82460a59096a7dda71
simple-ice <simplewu233@gmail.com>
2020-09-06 16:09:31 +0800

    渲染 Tabs 内容

--- src/components/demo/TabsDemo.vue
+++ src/components/demo/TabsDemo.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>Tabs 示例</div>
-  <h1>示例1</h1>
-  <Tabs>
+<div>Tabs 示例</div>
+<h1>示例1</h1>
+<Tabs>
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
-  </Tabs>
+</Tabs>
 </template>
 
 <script lang="ts">
 import Tab from "../../lib/Tab.vue";
 import Tabs from "../../lib/Tabs.vue";
 export default {
-  comments: {
-    Tab,
-    Tabs,
-  },
+    components: {
+        Tab,
+        Tabs,
+    },
 };
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
-        Tabs 组件
-    </div>
+<div>
+    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
+    <component v-for="(tab, index) in defaults" :key="index" :is="tab" />
+</div>
 </template>
+
+<script lang="ts">
+import Tab from "./Tab.vue";
+export default {
+    setup(props, context) {
+        const defaults = context.slots.default();
+        const titles: string[] = [];
+        defaults.forEach((tab) => {
+            if (tab.type !== Tab) {
+                throw new Error("Tabs 子标签必须为 Tab");
+            }
+            titles.push(tab.props.title);
+        });
+        return {
+            defaults,
+            titles,
+        };
+    },
+};
+</script>
