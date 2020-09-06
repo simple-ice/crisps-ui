2bff772631a967ad6b4b70d8795b93cfd4762e0e
simple-ice <simplewu233@gmail.com>
2020-09-06 16:24:39 +0800

    完成 Tabs 的css样式

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,13 @@
 <template>
-<div>
-    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(tab, index) in defaults" :key="index" :is="tab" />
+<div class="crisps-tabs">
+    <div class="crisps-tabs-nav">
+        <div class="crisps-tabs-nav-item" v-for="(t, index) in titles" :key="index">
+            {{ t }}
+        </div>
+    </div>
+    <div class="crisps-tabs-content">
+        <component class="crisps-tabs-content-item" v-for="(c, index) in defaults" :is="c" :key="index" />
+    </div>
 </div>
 </template>
 
@@ -24,3 +30,35 @@ export default {
     },
 };
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.crisps-tabs {
+    &-nav {
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+
+        &-item {
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+
+            &:first-child {
+                margin-left: 0;
+            }
+
+            &.selected {
+                color: $blue;
+            }
+        }
+    }
+
+    &-content {
+        padding: 8px 0;
+    }
+}
+</style>
