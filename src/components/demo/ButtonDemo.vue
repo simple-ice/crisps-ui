3cc253b5fe84c070535468e6ed0ad17c746ecaf6
simple-ice <simplewu233@gmail.com>
2020-09-08 10:06:22 +0800

    优化html架构

--- src/components/demo/ButtonDemo.vue
+++ src/components/demo/ButtonDemo.vue
@@ -1,53 +1,59 @@
 <template>
-<div>Button 组件</div>
-<h1>按钮类型</h1>
+<h1>Button 按钮</h1>
 <div>
+    <h2>按钮类型</h2>
     <Button>Default</Button>
     <Button theme="link">Link</Button>
     <Button theme="text">Text</Button>
 </div>
-<h1>按钮尺寸</h1>
+
 <div>
-    <Button size="big">Big</Button>
-    <Button>Normal</Button>
-    <Button size="small">Small</Button>
+    <h2>按钮尺寸</h2>
+    <div>
+        <Button size="big">Big</Button>
+        <Button>Normal</Button>
+        <Button size="small">Small</Button>
+    </div>
+    <div>
+        <Button size="big" theme="link">Big Link</Button>
+        <Button theme="link">Normal Link</Button>
+        <Button size="small" theme="link">Small Link</Button>
+    </div>
+    <div>
+        <Button size="big" theme="text">Big text</Button>
+        <Button theme="text">Normal text</Button>
+        <Button size="small" theme="text">Small text</Button>
+    </div>
 </div>
+
 <div>
-    <Button size="big" theme="link">Big Link</Button>
-    <Button theme="link">Normal Link</Button>
-    <Button size="small" theme="link">Small Link</Button>
-</div>
-<div>
-    <Button size="big" theme="text">Big text</Button>
-    <Button theme="text">Normal text</Button>
-    <Button size="small" theme="text">Small text</Button>
-</div>
-<h1>危险按钮</h1>
-<div>
-    <dir>
+    <h2>危险按钮</h2>
+    <div>
         <Button level="main">Primary</Button>
         <Button>Default</Button>
         <Button level="danger">Danger</Button>
-    </dir>
-    <dir>
+    </div>
+    <div>
         <Button theme="link" level="main">Primary link</Button>
         <Button theme="link">Default link</Button>
         <Button theme="link" level="danger">Danger link</Button>
-    </dir>
-    <dir>
+    </div>
+    <div>
         <Button theme="text" level="main">Primary text</Button>
         <Button theme="text">Default text</Button>
         <Button theme="text" level="danger">Danger text</Button>
-    </dir>
+    </div>
 </div>
-<h1>不可用状态</h1>
+
 <div>
+    <h2>不可用状态</h2>
     <Button disabled>Disabled</Button>
     <Button theme="link" disabled>Disabled link</Button>
     <Button theme="text" disabled>Disabled text</Button>
 </div>
-<h1>加载中状态</h1>
+
 <div>
+    <h2>加载中状态</h2>
     <Button loading>Loading</Button>
     <Button @click="onClick" :loading="isLoading ? true : false">Click me</Button>
 </div>
