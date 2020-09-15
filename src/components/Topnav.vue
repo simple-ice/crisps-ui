c274b2aedc41db74977cdbf4674428d05e743aa8
simple-ice <simplewu233@gmail.com>
2020-09-15 10:43:12 +0800

    整体背景色

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="logo" >LOGO</div>
+      <div class="logo">LOGO</div>
       <ul class="menu">
         <li>菜单1</li>
         <li>菜单2</li>
@@ -10,6 +10,7 @@
     </div>
   </div>
 </template>
+
 <script lang="ts">
 import { inject, Ref } from "vue";
 export default {
@@ -18,14 +19,15 @@ export default {
     const toggleAside = () => {
       asideVisible.value = !asideVisible.value;
     };
-    return { toggleAside };
+    return {
+      toggleAside,
+    };
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .topnav {
-  background: pink;
   display: flex;
   padding: 16px;
   position: fixed;
@@ -35,18 +37,22 @@ export default {
   z-index: 10;
   justify-content: center;
   align-items: center;
+
   > .logo {
     max-width: 6em;
     margin-right: auto;
   }
+
   > .menu {
     display: flex;
     white-space: nowrap;
     flex-wrap: nowrap;
+
     > li {
       margin: 0 1em;
     }
   }
+
   > .toggleAside {
     display: none;
     width: 24px;
@@ -57,15 +63,18 @@ export default {
     top: 50%;
     transform: translateY(-50%);
   }
+
   @media (max-width: 896px) {
     > .menu {
       display: none;
     }
+
     > .logo {
       margin: 0 auto;
     }
-    > .toggleAside{
-        display: inline-block;
+
+    > .toggleAside {
+      display: inline-block;
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,48 @@
 <template>
-  <div>
+<div class="topnavAndBanner">
     <Topnav />
     <div class="banner">
-      <h1>薯片UI</h1>
-      <h2>一个普普通通的 UI 框架</h2>
-      <p class="actions">
-        <a href="https://github.com/simple-ice/crisps-ui">GitHub</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+        <h1>薯片UI</h1>
+        <h2>一个普普通通的 UI 框架</h2>
+        <p class="actions">
+            <a href="https://github.com/simple-ice/crisps-ui">GitHub</a>
+            <router-link to="/doc">开始</router-link>
+        </p>
     </div>
-  </div>
+</div>
 </template>
+
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from "../components/Topnav.vue";
 export default {
-    name: 'Home',
-    components:{Topnav}
-}
+    name: "Home",
+    components: {
+        Topnav,
+    },
+};
 </script>
+
 <style lang="scss" scoped>
 .banner {
-  padding: 100px 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  background: lightgreen;
-  > .actions{
-    padding: 8px 0;
-    a {
-      margin:0 8px;
-      background: #fff;
-      display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    >.actions {
+        padding: 8px 0;
+
+        a {
+            margin: 0 8px;
+            background: #fff;
+            display: inline-block;
+            $h: 28px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h/2;
+            padding: 0 8px;
+        }
     }
-  }
 }
 </style>
