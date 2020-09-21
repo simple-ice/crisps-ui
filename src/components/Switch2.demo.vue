36aeaf8bcc54d7b81e180ef0f2552a28a30f17c3
simple-ice <simplewu233@gmail.com>
2020-09-21 18:34:00 +0800

    将用户需要复制的源代码抽离

---
+++ src/components/Switch2.demo.vue
@@ -0,0 +1,29 @@
+<template>
+<Switch v-model:value="bool1" size="big" />
+<br>
+<Switch v-model:value="bool2" />
+<br>
+<Switch v-model:value="bool3" size="small" />
+</template>
+
+<script lang="ts">
+import {
+    ref
+} from 'vue';
+import Switch from '../lib/Switch.vue';
+export default {
+    components: {
+        Switch
+    },
+    setup() {
+        const bool1 = ref(false);
+        const bool2 = ref(false);
+        const bool3 = ref(false);
+        return {
+            bool1,
+            bool2,
+            bool3,
+        }
+    }
+}
+</script>
--- src/components/demo/SwitchDemo.vue
+++ src/components/demo/SwitchDemo.vue
@@ -1,50 +1,103 @@
 <template>
 <div>
     <h1>Switch 开关</h1>
-    <h2>示例</h2>
-    <div>
-        <h3>基本</h3>
-        <Switch v-model:value="bool1" />
-    </div>
-    <div>
-        <h3>尺寸</h3>
-        <div>
-            <Switch v-model:value="bool2" size="big" />
-            <Switch v-model:value="bool3" />
-            <Switch v-model:value="bool4" size="small" />
+    <div class="demo">
+        <h2>基本</h2>
+        <div class="demo-component">
+            <Switch-1-demo />
+        </div>
+        <div class="demo-actions">
+            <Button>查看代码</Button>
+        </div>
+        <div class="demo-code">
+            <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
         </div>
     </div>
-    <div>
-        <h3>不可用状态</h3>
-        <div>
-            <Switch v-model:value="bool5" disabled/>
+    <div class="demo">
+        <h2>尺寸</h2>
+        <div class="demo-component">
+            <Switch-2-demo />
+        </div>
+
+        <div class="demo-actions">
+            <Button>查看代码</Button>
+        </div>
+        <div class="demo-code">
+            <pre>&lt;Switch v-model:value="bool" size="big" /&gt;</pre>
+            <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+            <pre>&lt;Switch v-model:value="bool" size="small" /&gt;</pre>
+        </div>
+    </div>
+    <div class="demo">
+        <h2>不可用状态</h2>
+        <div class="demo-component">
+            <Switch-3-demo />
+        </div>
+        <div class="demo-actions">
+            <Button>查看代码</Button>
+        </div>
+        <div class="demo-code">
+            <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
         </div>
     </div>
 </div>
 </template>
 
 <script lang="ts">
-import Switch from "../../lib/Switch.vue";
+import Button from "../../lib/Button.vue";
+import Switch1Demo from '../Switch1.demo.vue';
+import Switch2Demo from '../Switch2.demo.vue';
+import Switch3Demo from '../Switch3.demo.vue';
 import {
     ref
 } from "vue";
 export default {
     components: {
-        Switch,
+        Button,
+        Switch1Demo,
+        Switch2Demo,
+        Switch3Demo
     },
     setup() {
-        const bool1 = ref(false);
-        const bool2 = ref(false);
-        const bool3 = ref(false);
-        const bool4 = ref(false);
-        const bool5 = ref(false);
+
         return {
-            bool1,
-            bool2,
-            bool3,
-            bool4,
-            bool5,
+
         };
     },
 };
 </script>
+
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+
+.demo {
+    border: 1px solid $border-color;
+    margin: 16px 0 32px;
+
+    >h2 {
+        font-size: 20px;
+        padding: 8px 16px;
+        border-bottom: 1px solid $border-color;
+    }
+
+    &-component {
+        padding: 16px;
+    }
+
+    &-actions {
+        padding: 8px 16px;
+        border-top: 1px dashed $border-color;
+    }
+
+    &-code {
+        padding: 8px 16px;
+        border-top: 1px dashed $border-color;
+
+        >pre {
+            line-height: 1.1;
+            font-family: Consolas, 'Courier New', Courier, monospace;
+            margin: 0;
+        }
+    }
+}
+</style>
