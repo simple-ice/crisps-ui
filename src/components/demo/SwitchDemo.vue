f227344a7164e68e0dc8b967592b87dedd694688
simple-ice <simplewu233@gmail.com>
2020-09-22 14:09:08 +0800

    高亮显示源代码

--- src/components/demo/SwitchDemo.vue
+++ src/components/demo/SwitchDemo.vue
@@ -10,7 +10,7 @@
             <Button>查看代码</Button>
         </div>
         <div class="demo-code">
-            <pre>{{Switch1Demo.__sourceCode}}</pre>
+            <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')"></pre>
         </div>
     </div>
     <div class="demo">
@@ -22,7 +22,7 @@
             <Button>查看代码</Button>
         </div>
         <div class="demo-code">
-            <pre>{{Switch2Demo.__sourceCode}}</pre>
+            <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')"></pre>
         </div>
         <div class="demo">
             <h2>不可用状态</h2>
@@ -33,7 +33,7 @@
                 <Button>查看代码</Button>
             </div>
             <div class="demo-code">
-                <pre>{{Switch3Demo.__sourceCode}}</pre>
+                <pre class="language-html" v-html="Prism.highlight(Switch3Demo.__sourceCode, Prism.languages.html, 'html')"></pre>
             </div>
         </div>
     </div>
@@ -45,6 +45,9 @@ import Button from "../../lib/Button.vue";
 import Switch1Demo from '../Switch1.demo.vue';
 import Switch2Demo from '../Switch2.demo.vue';
 import Switch3Demo from '../Switch3.demo.vue';
+
+import Prism from 'prismjs';
+import 'prismjs/themes/prism.css';
 import {
     ref
 } from "vue";
@@ -57,7 +60,8 @@ export default {
         return {
             Switch1Demo,
             Switch2Demo,
-            Switch3Demo
+            Switch3Demo,
+            Prism
         };
     },
 };
