1a0b5e772a31f784d47eb8bf03cd4c632cee83bf
simple-ice <simplewu233@gmail.com>
2020-09-23 10:02:17 +0800

    优化 TabsDemo

--- src/components/demo/TabsDemo.vue
+++ src/components/demo/TabsDemo.vue
@@ -1,41 +1,21 @@
 <template>
 <h1>Tabs 标签页</h1>
-<h2>示例</h2>
-<div>
-    <h3>基本</h3>
-    <Tabs v-model:value="name1">
-        <Tab title="导航1">内容1</Tab>
-        <Tab title="导航2">内容2</Tab>
-        <Tab title="导航3">内容3</Tab>
-    </Tabs>
-</div>
-<div>
-    <h3>不可用状态</h3>
-    <Tabs v-model:value="name2">
-        <Tab title="导航1">内容1</Tab>
-        <Tab disabled title="导航2">内容2</Tab>
-        <Tab title="导航3">内容3</Tab>
-    </Tabs>
-</div>
+<Demo :component="Tabs1Demo" />
+<Demo :component="Tabs2Demo" />
 </template>
 
 <script lang="ts">
-import {
-    ref
-} from "vue";
-import Tab from "../../lib/Tab.vue";
-import Tabs from "../../lib/Tabs.vue";
+import Demo from '../Demo.vue';
+import Tabs1Demo from '../tabsExample/Tabs1.demo.vue';
+import Tabs2Demo from '../tabsExample/Tabs2.demo.vue';
 export default {
     components: {
-        Tab,
-        Tabs,
+        Demo
     },
     setup() {
-        const name1 = ref("导航1");
-        const name2 = ref("导航1");
         return {
-            name1,
-            name2,
+            Tabs1Demo,
+            Tabs2Demo,
         };
     },
 };
---
+++ src/components/tabsExample/Tabs2.demo.vue
@@ -0,0 +1,30 @@
+<demo>不可用状态</demo>
+<template>
+<div>
+    <Tabs v-model:value="name">
+        <Tab title="导航1">内容1</Tab>
+        <Tab disabled title="导航2">内容2</Tab>
+        <Tab title="导航3">内容3</Tab>
+    </Tabs>
+</div>
+</template>
+
+<script lang="ts">
+import {
+    ref
+} from 'vue';
+import Tab from '../../lib/Tab.vue';
+import Tabs from '../../lib/Tabs.vue';
+export default {
+    components: {
+        Tab,
+        Tabs
+    },
+    setup() {
+        const name = ref("导航1");
+        return {
+            name
+        }
+    }
+}
+</script>
